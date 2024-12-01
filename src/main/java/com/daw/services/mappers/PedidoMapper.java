fa74ff1d4b3443deb59b4ade5250111364844158
dmarlop <davidmarinlpz@gmail.com>
2024-12-01 13:13:18 +0100

    mappers de pedido y pizzaPedido

--- src/main/java/com/daw/services/mappers/PedidoMapper.java
+++ src/main/java/com/daw/services/mappers/PedidoMapper.java
@@ -1,5 +1,25 @@
 package com.daw.services.mappers;
 
+import com.daw.persistence.entities.Pedido;
+import com.daw.services.dto.PedidoDTO;
+
 public class PedidoMapper {
+	
+	public static PedidoDTO toDto (Pedido pedido) {
+		PedidoDTO dto = new PedidoDTO();
+		
+		dto.setCliente(pedido.getCliente().toString());
+		dto.setDireccion(pedido.getCliente().getDireccion());
+		dto.setFecha(pedido.getFecha());
+		dto.setId(pedido.getId());
+		dto.setMetodo(pedido.getMetodo());
+		dto.setNotas(pedido.getNotas());
+		//dto.setPizzas(pedido.getPizzaPedido().getFirst());
+		dto.setTelefono(pedido.getCliente().getTelefono());
+		dto.setTotal(pedido.getTotal());
+		
+		return dto;
+		
+	}
 
 }
--- src/main/java/com/daw/services/mappers/PizzaPedidoMapper.java
+++ src/main/java/com/daw/services/mappers/PizzaPedidoMapper.java
@@ -1,5 +1,19 @@
 package com.daw.services.mappers;
 
+import com.daw.persistence.entities.PizzaPedido;
+import com.daw.services.dto.PizzaPedidoOutputDTO;
+
 public class PizzaPedidoMapper {
+	
+	public static PizzaPedidoOutputDTO toDto (PizzaPedido pizzaPedido) {
+		PizzaPedidoOutputDTO dto = new PizzaPedidoOutputDTO();
+		dto.setId(pizzaPedido.getId());
+		dto.setIdPizza(pizzaPedido.getIdPizza());
+		dto.setCantidad(pizzaPedido.getCantidad());
+		dto.setPrecio(pizzaPedido.getPrecio());
+		dto.setPizza(pizzaPedido.getPizza().getNombre());		
+		
+		return dto;
+	}
 
 }
